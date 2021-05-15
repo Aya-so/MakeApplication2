45e7cd76db70d82730b1318f85efef721dfa0d4b
Aya-so <ayaka.wakame0315@gmail.com>
2021-05-15 15:01:38 +0000

    [Add]コメント機能・Fav機能追加

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '32e6a3872098f5d43f2106a49cda07f45778297b1bf78b9a9edd0359b9fdd0c1b82e36eaba1bd181a3a7b738002a7e9ce3ac067ece34d3163d9595134e3b4a3b'
